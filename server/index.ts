09ed0f4d5467f551dd16a847bb5a512c8e3a2463
Yunus Ahmed Choudhury <yunuschoudhurywork@gmail.com>
2026-07-14 02:00:24 +0600

    log.console updated

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,22 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-console.log("API KEY:",
-process.env.WORKOS_API_KEY);
-console.log("CLIENT ID:",
-process.env.WORKOS_CLIENT_ID);
-//fixing issues above
 
-
-console.log("Backend started");
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/auth';
-import userRoutes from "./routes/user";// phase 2 for admin and client
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth";
+import userRoutes from "./routes/user";
 
 const app = express();
-app.use (cors()); //to enable cors
+
+app.use(cors());
 app.use(express.json());
-app.use("/api/users",userRoutes);
-app.use('/api/auth',authRoutes); //admin and client.
 
-const PORT = process.env.PORT || 3000; //process.env.PORT
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`); //little display issues might coome.
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port $ {port}'); //only startup log
+
 });
