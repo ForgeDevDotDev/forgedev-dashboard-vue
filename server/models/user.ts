86031649700d50dae70ef44f334fcb245f5b2c46
Yunus Ahmed Choudhury <yunuschoudhurywork@gmail.com>
2026-07-13 22:53:33 +0600

    User data model/interface for backend

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,1 +1,20 @@
-//name, email, avatar, track, domain, tier, startDate,cohort..
+//user data
+export interface User {
+  id?: string;
+  name: string;
+  email: string;
+  avatar?: string;
+
+  role: "admin" | "client";
+
+  track?: string;
+  domain?: string;
+  tier?: string;
+  cohort?: string;
+
+  startDate?: string;
+  endDate?: string;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
